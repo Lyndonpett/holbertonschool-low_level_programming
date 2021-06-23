53ca5a5507b395a42ba9b2abc729b443cce72325
lyndonpett <lyndonpett98@gmail.com>
2021-06-23 08:42:11 -0700

    Added file 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints \"Programming is like building a multilingual puzzle
+ *
+ * Return: 0 after execution
+ */
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle");
+getchar();
+return (0);
+}
