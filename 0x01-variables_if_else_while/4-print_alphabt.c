3531f8ff691abd339ce0012ebbe0f16dd3caae2c
lyndonpett <lyndonpett98@gmail.com>
2021-06-25 09:37:19 -0700

    Added file 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet in lowercase.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	while (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	putchar (ch);
+	}
+	putchar ('\n');
+	return (0);
+}i
