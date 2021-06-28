7924904e2b453c2a5daf93e69b1de62ca76a1e3e
lyndonpett <lyndonpett98@gmail.com>
2021-06-28 08:37:41 -0700

    Added file 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Prints alphabet in lowercase.
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	_putchar (ch);
+	}
+	_putchar('\n');
+}
