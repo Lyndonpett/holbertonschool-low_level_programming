a5484f64dc4dcd03563db887dadbad35dc70f042
lyndonpett <lyndonpett98@gmail.com>
2021-06-28 11:34:03 -0700

    Added file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - Adds two integers and returns the result.
+ *
+ * @num1: The character of one number.
+ * @num2: The character of the second number.
+ *
+ * Return: The result of the two numbers.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
