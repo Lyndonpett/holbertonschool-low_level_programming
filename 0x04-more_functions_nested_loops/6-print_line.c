c8c6fc8bace407eb936986c0e95ac5353b6a18e9
lyndonpett <lyndonpett98@gmail.com>
2021-07-01 09:44:42 -0700

    Added file 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_line - prints a line in the terminal.
+ *
+ * @n: number of times the character to be printed.
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int line;
+
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+			_putchar('_');
+	}
+	_putchar('\n');
+
+}
