709848f7cb6335cb5ac48194b3a45d924f3d19c9
lyndonpett <lyndonpett98@gmail.com>
2021-07-01 12:18:19 -0700

    Added file 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,25 +17,22 @@ int main(void)
 		if (i % 15 == 0)
 		{
 			printf("FizzBuzz");
-			putchar(' ');
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			putchar(' ');
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			putchar(' ');
 		}
 		else
 		{
 			printf("%d", i);
-			putchar(' ');
 		}
 		if (i == 100)
 		continue;
+		putchar(' ');
 	}
 	printf("\n");
 	return (0);
