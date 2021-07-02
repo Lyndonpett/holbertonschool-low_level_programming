3c4ff33e19c56e8745ce3befdd962e666340e827
lyndonpett <lyndonpett98@gmail.com>
2021-07-02 07:32:14 -0700

    Added file 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+* swap_int - swaps two integers.
+*
+* @a: first integer
+* @b: second integer
+*
+* Return: 0
+*/
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
