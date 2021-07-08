aa60b548332b699a3a0c3b88d3997332a24bcb99
lyndonpett <lyndonpett98@gmail.com>
2021-07-08 10:49:09 -0700

    Added file 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include "2-strlen.c"
+/**
+ * reverse_array - reverses an array.
+ *
+ * @a: the array being reversed.
+ * @n the number of elements of the array.
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int low, high;
+
+	for (low = 0, high = n - 1; low < high; low++, high--)
+	{
+		int temp = a[low];
+		a[low] = a[high];
+		a[high] = temp;
+	}
+}
