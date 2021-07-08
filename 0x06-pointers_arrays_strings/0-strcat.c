0fb0dae4164773b94aace3abb470683c8da91332
lyndonpett <lyndonpett98@gmail.com>
2021-07-08 09:00:02 -0700

    Fixed betty for 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,16 +3,16 @@
 /**
  * _strcat - concatenates two strings
  *
- * @*dest: First string.
- * @*src: Second string.
+ * @dest: First string.
+ * @src: Second string.
  *
  * Return: Dest.
  */
 char *_strcat(char *dest, char *src)
 {
-	char* ptr = dest + _strlen(dest);
+	char *ptr = dest + _strlen(dest);
 
-	while(*src != '\0')
+	while (*src != '\0')
 	{
 		*ptr++ = *src++;
 	}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include "2-strlen.c"
+/**
+ * _strncat - concatenates two strings.
+ *
+ * @*dest: first string.
+ * @*src: second string.
+ * @n: byte size.
+ *
+ * Return: dest
+ *
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	
