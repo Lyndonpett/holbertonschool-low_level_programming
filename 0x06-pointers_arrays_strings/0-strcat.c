b1120e142bcd2572594d5d3e36c4d88c761a9407
lyndonpett <lyndonpett98@gmail.com>
2021-07-08 08:54:18 -0700

    Added file 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include "2-strlen.c"
+/**
+ * _strcat - concatenates two strings
+ *
+ * @*dest: First string.
+ * @*src: Second string.
+ *
+ * Return: Dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char* ptr = dest + _strlen(dest);
+
+	while(*src != '\0')
+	{
+		*ptr++ = *src++;
+	}
+
+	*ptr = '\0';
+
+	return (dest);
+}
+
