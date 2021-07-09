f5ff2381bcb06bb872b6e4bfea42d71ac1bf16e0
lyndonpett <lyndonpett98@gmail.com>
2021-07-09 08:01:04 -0700

    Added file 3

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -5,6 +5,7 @@ int _strlen(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 
 #endif /*HOLBERTON_H*/
