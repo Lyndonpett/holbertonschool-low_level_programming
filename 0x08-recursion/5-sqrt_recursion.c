5375802067741ca2600a08db20ff888efc34ee8d
lyndonpett <lyndonpett98@gmail.com>
2021-07-12 10:55:22 -0700

    Added file 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * sqrtHELPER - helps find the square root of a number.
  *
@@ -10,21 +11,17 @@
  */
 int sqrtHELPER(int goal, int squirt)
 {
-	if (goal == 1)
-	{
-		return (1);
-	}
-	else if (squirt <= 0)
-	{
-		return (-1);
-	}
-	else if (squirt * squirt == goal)
+	if ((goal / squirt) == squirt && (goal / squirt) * (goal / squirt) == goal)
 	{
 		return (squirt);
 	}
+	else if (squirt > goal / 2)
+	{
+		return (-1);
+	}
 	else
 	{
-		return (sqrtHELPER(goal, squirt - 1));
+		return ((sqrtHELPER(goal, squirt + 1)));
 	}
 }
 /**
@@ -36,7 +33,20 @@ int sqrtHELPER(int goal, int squirt)
  */
 int _sqrt_recursion(int n)
 {
-	return (sqrtHELPER(n, n / 2));
+	int goal;
+
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+
+	goal = 1;
+
+	return (sqrtHELPER(n, goal));
 }
 
 
