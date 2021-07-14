d30bda29a197d585b52782a1ca7173e63584cf0c
lyndonpett <lyndonpett98@gmail.com>
2021-07-14 11:29:21 -0500

    Fixed betty for file 0

--- 0x0B-argc_argv/0-whatsmyname.c
+++ 0x0B-argc_argv/0-whatsmyname.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+/**
+ * main - prints name of a program
+ *
+ * @argc: unused parameter
+ * @argv: the name of the program
+ *
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
-    argc = argc;
-    printf("%s\n", argv[0]);
-    return (0);
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+
 }
