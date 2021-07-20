3f0032e91a7aaa318924252eceff4e968f048e0f
lyndonpett <lyndonpett98@gmail.com>
2021-07-20 09:53:46 -0500

    Added file 2

---
+++ 0x0C-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * _strdup -  returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ *
+ * @str: the string being copied.
+ *
+ * Return: NULL if string is NULL, On success, returns the
+ * string or if it fails, returns NULL.
+ */
+
+char *_strdup(char *str)
+{
+	char *src, *p;
+	int len = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[len])
+	{
+		len++;
+	}
+	src = malloc(len + 1);
+	p = src;
+
+	while (*str)
+	{
+		*p++ = *str++;
+	}
+	*p = '\0';
+	return (src);
+}
--- 0x0C-malloc_free/holberton.h
+++ 0x0C-malloc_free/holberton.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-char *create_array(unsigned int size, char c);
 int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 
 
