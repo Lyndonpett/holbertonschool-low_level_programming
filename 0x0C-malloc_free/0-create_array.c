c9ba47fc4fd102349342631b984db5ba96201f05
lyndonpett <lyndonpett98@gmail.com>
2021-07-20 09:34:40 -0500

    Added file 1

---
+++ 0x0C-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * create_array - creates an array of chars, and initializes it
+ * with a specific char.
+ *
+ * @size: the size of the array.
+ * @c: the character to initialize the array.
+ *
+ * Return: NULL, if size = 0 or a pointer to the array, or null if that fails.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	a = malloc(size);
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i > size; i++)
+	{
+		a[i] = c;
+	}
+	return (a);
+}
--- 0x0C-malloc_free/holberton.h
+++ 0x0C-malloc_free/holberton.h
@@ -1,5 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
 
