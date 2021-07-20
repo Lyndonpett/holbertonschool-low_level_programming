0138b543e1146c97b55fb1b3d19c5a1141cf6a29
lyndonpett <lyndonpett98@gmail.com>
2021-07-20 11:04:44 -0500

    Added file 3

--- 0x0C-malloc_free/3-alloc_grid.c
+++ 0x0C-malloc_free/3-alloc_grid.c
@@ -26,18 +26,13 @@ int **alloc_grid(int width, int height)
 
 	if (array == NULL)
 	{
+		free(array);
 		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
 	{
 		array[i] = malloc(width * sizeof(int));
-
-		if (array[i] == NULL)
-		{
-			free(array);
-			return (NULL);
-		}
 	}
 	return (array);
 }
