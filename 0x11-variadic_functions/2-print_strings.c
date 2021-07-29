47fb06dc353c8e1dbbf25e7675478c06edd613c0
lyndonpett <lyndonpett98@gmail.com>
2021-07-29 12:10:43 -0500

    File 2, changed nill to nil

--- 0x11-variadic_functions/2-print_strings.c
+++ 0x11-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		if (va_arg(strcheck, char *) == NULL)
 		{
-			printf("(nill)");
+			printf("(nil)");
 			va_arg(yeet, char *);
 		}
 
