32d5ad36326de80907c516f0e38fa72fc9120a21
lyndonpett <lyndonpett98@gmail.com>
2021-07-29 09:03:46 -0500

    Added file 0

---
+++ 0x11-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - adds all numbers passed through function.
+ *
+ * @n: the numbers being passed.
+ *
+ * Return: the sum of the ints.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list yeet;
+
+	va_start(yeet, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(yeet, int);
+	}
+	va_end(yeet);
+
+	return (sum);
+}
