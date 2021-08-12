6cb503580ba2460d154e2396f330b8b5c3e14918
lyndonpett <lyndonpett98@gmail.com>
2021-08-12 10:26:35 -0500

    Added file 5-flip_bits

---
+++ 0x15-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * countSetBits - counts the bits to be set.
+ *
+ * @n: the number to reset.
+ *
+ * Return: 1 or -1.
+ */
+
+int countSetBits(int n)
+{
+	int count = 0;
+
+	while (n > 0)
+	{
+		count++;
+		n &= (n - 1);
+	}
+	return (count);
+}
+
+/**
+ * flip_bits - counts the bits of a flip.
+ *
+ * @n: the number to be reset.
+ * @m: index to change.
+ *
+ * Return: 1 or -1.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return countSetBits(n ^ m);
+}
