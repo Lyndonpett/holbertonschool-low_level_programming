c57f5669bfbc25d46ae86863176938fc9859fbf2
lyndonpett <lyndonpett98@gmail.com>
2021-08-12 10:14:44 -0500

    Added file 4-clear_bit

---
+++ 0x15-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	{
+		return (-1);
+	}
+	*n = *n & (~(1 << index));
+	return (1);
+}
