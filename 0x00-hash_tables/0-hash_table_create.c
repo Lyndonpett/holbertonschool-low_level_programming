82e042b5ba788b54e3d1a7b6720d53a2c828db3b
lyndonpett <lyndonpett98@gmail.com>
2022-02-16 10:44:38 -0600

    reverting if check

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashTable;
 
-	if (size == NULL)
+	if (!size)
 		return (NULL);
 
 	hashTable = malloc(sizeof(hash_node_t));
