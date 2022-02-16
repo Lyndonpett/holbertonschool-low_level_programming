d167c438e1e9ea0fe70c2ef0460317b69f391191
lyndonpett <lyndonpett98@gmail.com>
2022-02-16 10:49:38 -0600

    Trying hope's way

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -2,35 +2,27 @@
 
 /**
  * hash_table_create - creates a hash table
- *
  * @size: size of the array
  *
- * Return: pointer to new hash table
+ * Return: pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hashTable;
 	unsigned long int i;
+	hash_table_t *table;
 
-	if (!size)
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
 		return (NULL);
-
-	hashTable = malloc(sizeof(hash_node_t));
-	if (hashTable == NULL)
-		return (NULL);
-
-	hashTable->size = size;
-	hashTable->array = calloc(size, sizeof(hash_node_t *));
-
-	if (!hashTable)
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!table->array)
 	{
-		free(hashTable);
+		free(table);
 		return (NULL);
 	}
-	while (i < size)
-	{
-		hashTable->array[i++] = NULL;
-	}
+	table->size = size;
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
-	return (hashTable);
+	return (table);
 }
