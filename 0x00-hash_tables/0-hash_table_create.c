520e217563cc0cb654cb140a416a59af698a9756
lyndonpett <lyndonpett98@gmail.com>
2022-02-16 10:07:08 -0600

    Adding task 0

---
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ *
+ * @size: size of the array
+ *
+ * Return: pointer to new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashTable = NULL;
+
+	hashTable = malloc(sizeof(hash_node_t));
+	if (hashTable == NULL)
+		return (NULL);
+
+	hashTable->size = size;
+	hashTable->array = calloc(size, sizeof(hash_node_t *));
+
+	if (!hashTable->array)
+		return (NULL);
+
+	return (hashTable);
+}
