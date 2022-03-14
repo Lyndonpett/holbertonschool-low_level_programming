24eb10bbc184f8cb35d71efcae0129a74b5070e8
lyndonpett <lyndonpett98@gmail.com>
2022-03-14 11:15:27 -0500

    Adding task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void printArray(int *array, int left, int right);
 
 
 #endif
